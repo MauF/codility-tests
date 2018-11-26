57d1d9948a3cf841a161d3fce48ab29b5405afc6
MauF <maurizio.fassone@gmail.com>
2018-11-27 00:02:57 +0100

    fixes

---
+++ src/test/java/com/mau/test26112018/Test.java
@@ -0,0 +1,18 @@
+package com.mau.test26112018;
+
+import org.junit.jupiter.api.Assertions;
+
+public class Test {
+
+//        Assertions.assertEquals(2,s.numberOfCarryOperations(65,55));
+//        Assertions.assertEquals(0,s.numberOfCarryOperations(123,456)); // 0
+//        Assertions.assertEquals(3,s.numberOfCarryOperations(555,555)); // 3
+//        Assertions.assertEquals(1,s.numberOfCarryOperations(101,809)); // 1
+//        Assertions.assertEquals(1,s.numberOfCarryOperations(189,209)); // 1
+//        Assertions.assertEquals(0,s.numberOfCarryOperations(0,0)); // 0
+//        Assertions.assertEquals(0,s.numberOfCarryOperations(900,11));// 0
+//        Assertions.assertEquals(2,s.numberOfCarryOperations(145,55));// 2
+//        Assertions.assertEquals(5,s.numberOfCarryOperations(1,99999)); // 5
+//        Assertions.assertEquals(5,s.numberOfCarryOperations(999045,1055)); // 5
+
+}
